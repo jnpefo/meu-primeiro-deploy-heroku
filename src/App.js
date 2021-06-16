80494bdfb926207abeda227eb6935f334fce365e
jnpefo <jnpefo@gmail.com>
2021-06-16 18:13:00 -0300

    teste 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Franco <code>Caju</code> Jessandro.
         </p>
         <a
           className="App-link"
